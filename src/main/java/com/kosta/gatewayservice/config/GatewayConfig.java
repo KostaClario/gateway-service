96a4c3ef13519d7504066e5cf97afebf6f2b3057
taebin5824 <xoqls5824@gmail.com>
2025-07-10 00:19:22 +0900

    feat: gateway 라우팅 설정

---
+++ src/main/java/com/kosta/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,29 @@
+package com.kosta.gatewayservice.config;
+
+import com.kosta.gatewayservice.auth.jwt.JwtAuthorizationFilter;
+import com.kosta.gatewayservice.filter.LoggingGatewayFilterFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class GatewayConfig {
+
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder, LoggingGatewayFilterFactory loggingGatewayFilterFactory) {
+        return builder.routes()
+                .route("user-service", r -> r
+                        .path("/oauth2/**", "/login/**", "/api/member", "/token/**")
+                        .filters(f -> f
+                                .filter(jwtAuthorizationFilter)
+                                .filter(loggingGatewayFilterFactory.apply(new LoggingGatewayFilterFactory.Config()))
+                        )
+                        .uri("lb://user-service"))
+                .build();
+    }
+}
